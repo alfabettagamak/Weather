13d94663839bd7b3809ac001910b28ac2b6bb0a1
alfabettagamak <alisa.skrynko@gmail.com>
2014-08-24 15:24:52 +0700

    db

--- weather.php
+++ weather.php
@@ -30,10 +30,40 @@ $result = json_decode($result, true);
 $result = $result[result];
 echo '<p>Decode</p>'; 
 print_r($result);
-echo "\nТемпература : $result[temp_current_c]"; 
-echo iconv('Windows-1251', 'UTF-8', "\n $result[cloud_title]"); 
-echo "$result[precip_title]"; 
-echo "\nДавление : $result[pressure_avg] мм " ; 
-echo "\nВлажность : $result[humidity_avg] % "; 
-echo "\nСкорость ветра : $result[wind_avg] м/с"; 
+#echo "\nТемпература : $result[temp_current_c]"; 
+#echo "\n $result[cloud_title] "; 
+#echo "$result[precip_title]"; 
+#echo "\nДавление : $result[pressure_avg] мм " ; 
+#echo "\nВлажность : $result[humidity_avg] % "; 
+echo "\Параметры:"; 
+
+ $res = array(
+ 'city' => $params[city],
+ 'time' => date("d.m.Y G:i:s"),
+ 'temp_current_c' => $result[temp_current_c],
+ 'pressure_avg' => $result[pressure_avg],
+ 'humidity_avg' => $result[humidity_avg],
+ 'wind_avg' => $result[wind_avg],
+ );
+ print_r($res);
+?>
+
+<?php 
+try {
+ $conn = new Mongo('185.31.161.49');
+
+ $db = $conn->test;
+ $collection = $db->weather;
+
+
+ $collection->insert($res);
+ 
+ echo 'Inserted document with ID: ' . $weather['_id'];
+ // disconnect from server
+ $conn->close();
+} catch (MongoConnectionException $e) {
+ die('Error connecting to MongoDB server');
+} catch (MongoException $e) {
+ die('Error: ' . $e->getMessage());
+}
 ?>
